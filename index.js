6e1c0cee2d7f46fb62cb3faf04b056b580802395
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2023-03-17 21:35:02 +0100

    Improve error management and logs

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 import Koa from "koa";
-import * as createError from "http-errors";
 
 import accesslog from "./lib/middlewares/log.js";
 import proxy from "./lib/middlewares/proxy.js";
 import health from "./lib/middlewares/health.js";
+import error from "./lib/middlewares/error.js";
 
 import { PORT } from "./lib/config.js";
 
 const app = new Koa();
 
 app.use(accesslog);
-
+app.use(error);
 app.use(health);
-
 app.use(proxy);
 
 app.listen(PORT);
-
-app.on("error", (err, ctx) => {
-  if (createError.isHttpError(err) && err.status < 500) {
-    // Ignore 40* errors, they aren't unexpected behaviour
-    return;
-  }
-
-  // Probably a remote request error
-  if (err.timings) {
-    console.log(`Error while handling remote request '${ctx.method} ${ctx.path}':\n`, err.name, err.message);
-    return;
-  }
-
-  console.log(`Error while handling '${ctx.method} ${ctx.path}':\n`, err);
-});
 
 console.log("Listening on port", PORT);
